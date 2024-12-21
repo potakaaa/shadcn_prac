a1aeace3569850c0290c6f0a8cbfd5f9efb342c9
potakaaa <helbirog@gmail.com>
2024-12-21 11:26:15 +0800

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex size-full bg-zinc-800 items-center justify-center h-screen">
+      <Button className="flex w-32 h-14 rounded-full">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
